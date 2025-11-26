2df4cfe4ee928b09c82c2688c1f793e3280b413c
Dhruv316 <dhruvrane88306@gmail.com>
2025-11-27 02:45:02 +0530

    added new firebase config

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,24 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD8OeoWmR5TWqod7SOH89yuY5wHTxEFSAI",
-  authDomain: "scan-the-lie.firebaseapp.com",
-  projectId: "scan-the-lie",
-  storageBucket: "scan-the-lie.firebasestorage.app",
-  messagingSenderId: "299788260655",
-  appId: "1:299788260655:web:f8c46ff570c0a25ff67dda"
+  apiKey: "AIzaSyDWMJfh5wrDxoVuFrvLldHSHCEgh42wFks",
+  authDomain: "scan-the-lie-cba00.firebaseapp.com",
+  projectId: "scan-the-lie-cba00",
+  storageBucket: "scan-the-lie-cba00.firebasestorage.app",
+  messagingSenderId: "418582353520",
+  appId: "1:418582353520:web:7d7ce17faaee4cc4fe06b5",
+  measurementId: "G-JXCV582R81"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
-export const auth = getAuth(app);
-export const googleProvider = new GoogleAuthProvider();
-
-// Initialize Cloud Firestore and get a reference to the service
-export const db = getFirestore(app);
-
-export default app; 
+const analytics = getAnalytics(app);
